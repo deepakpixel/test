40891df5c900e63194bb79504f8bb86d59eba1bc
deepakpixel <49981613+deepakpixel@users.noreply.github.com>
2020-12-10 22:24:34 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
-var x = document.createElement('div');
-x.style.backgroundColor
-x.style.position='fixed';
-x.style.top=0
-x.style.left=0
-x.innerText='I AM YOUR DIV'
+var d_helper = document.createElement('div');
+var d_style="position:fixed;top:0;left:0;color:#fff;background:#222;padding:5px 1em;font:14px sans-serif;z-index:999999";
+
+d_helper.innerHTML=`<div class="d-helper">
+  <strong>
+    Yooo... mf, I am in, you piece of sh*t
+  </strong>
+  <p>
+    Lorem ipsum laga denge ab to sabke xddd
+  </p>
+</div>`
 document.body.append(x)
+
+
+
+
+
+if (typeof b.style.cssText !== "undefined") {
+    b.style.cssText = c;
+  } else {
+    b.setAttribute("style", c);
+  }
